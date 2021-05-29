4df33d7f2c0fab24ef89bd96b9b71e3bbbf835de
byken <32236865+bykenx@users.noreply.github.com>
2021-05-29 16:28:50 +0800

    rename "convertToDataUrl" to "toFormData"

--- __tests__/fileTool.spec.ts
+++ __tests__/fileTool.spec.ts
@@ -7,7 +7,7 @@ describe('test file tool', () => {
   test('test file convert to data url', async () => {
     const buffer = readFileSync(join(__dirname, 'fixtures/test.jpg'))
     const file = new File([new Uint8Array(buffer)], 'test.jpg', { type: 'image/jpeg' })
-    await expect(FileTool.convertToDataUrl(file)).resolves.toEqual(`data:image/jpeg;base64,${buffer.toString('base64')}`)
+    await expect(FileTool.toDataUrl(file)).resolves.toEqual(`data:image/jpeg;base64,${buffer.toString('base64')}`)
   })
 
   // TODO: to test fromBuffer
--- src/fileTool.ts
+++ src/fileTool.ts
@@ -1,12 +1,24 @@
 import { Completer } from "./completer"
 import { fromBuffer as fileTypeFromBuffer } from "file-type"
 
+type BufferLikeType = Buffer | ArrayBufferLike | Uint8Array
+
+const mimeTypeRe = /data:([^;]+)/
+
 /**
  * 文件处理工具
  */
 export class FileTool {
+
+  /** `Buffer` 或 `ArrayBufferLike` 转 `File` */
+  static async fromBuffer(buffer: BufferLikeType, fileName: string) {
+    const result = await fileTypeFromBuffer(buffer)
+    const type = result?.mime || undefined
+    return new File([new Uint8Array(buffer)], fileName, { type })
+  }
+
   /** `File` 转 `DataURL` */
-  static convertToDataUrl(file: File) {
+  static toDataUrl(file: File) {
     const reader = new FileReader()
     const completer = new Completer<string>()
     reader.addEventListener('load', (event) => {
@@ -16,16 +28,8 @@ export class FileTool {
     return completer.future
   }
 
-  /** `Buffer` 或 `ArrayBufferLike` 转 `File` */
-  static async fromBuffer(buffer: Buffer | ArrayBufferLike, fileName: string) {
-    const result = await fileTypeFromBuffer(buffer)
-    const type = result?.mime || undefined
-    return new File([new Uint8Array(buffer)], fileName, { type })
-  }
-
   /** `File` 转 `FormData` */
-  static async toFormData(file: File | File[], fieldName: string | undefined) {
-    fieldName = fieldName || 'file'
+  static async toFormData(file: File | File[], fieldName: string = 'file') {
     const formData = new FormData()
     const files = (file as any).length > 0 ? file as File[] : [file as File]
     for (let file of files) {
